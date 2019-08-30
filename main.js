8f321e0d3b668e8bc7d98253e674f90fdbb455c0
nightyknite <nightyknite@gmail.com>
2019-08-30 23:55:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,76 +1,95 @@
+document.addEventListener("DOMContentLoaded", function(){
 
-    
-  $(function () {
 
-    var setCalendar = function(start, end,callback){
-      var events = [];
-      var ym = start.add(7, 'days').format("YYYYMM"); 
-      var item = sessionStorage.getItem('event' + ym);
+    function setCalendar(start, end, callback){
+      let events = [];
+      const ym = start.add(7, 'days').format("YYYYMM"); 
+      /*
+      const item = sessionStorage.getItem('event' + ym);
       if (item) {
         events = JSON.parse(item);
         callback(events);
         return;
       }
-      $.when(
-        $.ajax({
-          url: "https://connpass.com/api/v1/event/?count=100&ym="+ym+"",
-          dataType: 'jsonp',
-        }),
-        $.ajax({
-          url: "https://api.atnd.org/events/?count=100&ym="+ym+"&start=1&format=jsonp",
-          dataType: 'jsonp',
-        })
-      )
-      .done(function(data1, data2) {
+      */
+      const connpass = data => {
+        let events = null;
+        for (var i in data[0].events) {
+          events.push({
+            title: data[0].events[i].title,
+            start: moment(data[0].events[i].started_at),
+            end: moment(data[0].events[i].ended_at),
+            url: data[0].events[i].event_url,
+            description: data[0].events[i].catch,
+            description: ""
+                         + "day:" + moment(data[0].events[i].started_at).format("MM/DD HH:mm") + " - "
+                         + "" + moment(data[0].events[i].ended_at).format("MM/DD HH:mm") + "<br>"
+                         + "limit:" + data[0].events[i].limit + "<br>"
+                         + "place:" + data[0].events[i].place + "<br>"
+                         + "address:" + data[0].events[i].address + "<br>"
+                         + "description:" + data[0].events[i].catch.substring(0,49) + "<br>"
+                         + "",
 
-        for (var i in data1[0].events) {
-            events.push({
-              title: data1[0].events[i].title,
-              start: moment(data1[0].events[i].started_at),
-              end: moment(data1[0].events[i].ended_at),
-              url: data1[0].events[i].event_url,
-              description: data1[0].events[i].catch,
-              description: ""
-                           + "day:" + moment(data1[0].events[i].started_at).format("MM/DD HH:mm") + " - "
-                           + "" + moment(data1[0].events[i].ended_at).format("MM/DD HH:mm") + "<br>"
-                           + "limit:" + data1[0].events[i].limit + "<br>"
-                           + "place:" + data1[0].events[i].place + "<br>"
-                           + "address:" + data1[0].events[i].address + "<br>"
-                           + "description:" + data1[0].events[i].catch.substring(0,49) + "<br>"
-                           + "",
+            backgroundColor: '#a82400',
+            borderColor: '#a82400'
+          });
+        }
+        return events;
+      }
 
-              backgroundColor: '#a82400',
-              borderColor: '#a82400'
-            });
-          
-          }
+      const atnd = data => {
+        let events = null;
+        for (var i in data[0].events) {
+          events.push({
+            title: data[0].events[i].event.title,
+            start: moment(data[0].events[i].event.started_at),
+            end: moment(data[0].events[i].event.ended_at),
+            url: data[0].events[i].event.event_url,
+            description: ""
+                         + "day:" + moment(data[0].events[i].event.started_at).format("MM/DD HH:mm") + " - "
+                         + "" + moment(data[0].events[i].event.ended_at).format("MM/DD HH:mm") + "<br>"
+                         + "limit:" + data[0].events[i].event.limit + "<br>"
+                         + "place:" + data[0].events[i].event.place + "<br>"
+                         + "address:" + data[0].events[i].event.address + "<br>"
+                         + "description:" + data[0].events[i].event.description.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'').substring(0,49) + "<br>"
+                         + "",
+            backgroundColor: '#EBAC2B',
+            borderColor: '#EBAC2B'
+          });
+        }
+        return events;
+      }
 
-          for (var i in data2[0].events) {
-            events.push({
-              title: data2[0].events[i].event.title,
-              start: moment(data2[0].events[i].event.started_at),
-              end: moment(data2[0].events[i].event.ended_at),
-              url: data2[0].events[i].event.event_url,
-              description: ""
-                           + "day:" + moment(data2[0].events[i].event.started_at).format("MM/DD HH:mm") + " - "
-                           + "" + moment(data2[0].events[i].event.ended_at).format("MM/DD HH:mm") + "<br>"
-                           + "limit:" + data2[0].events[i].event.limit + "<br>"
-                           + "place:" + data2[0].events[i].event.place + "<br>"
-                           + "address:" + data2[0].events[i].event.address + "<br>"
-                           + "description:" + data2[0].events[i].event.description.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'').substring(0,49) + "<br>"
-                           + "",
-              backgroundColor: '#EBAC2B',
-              borderColor: '#EBAC2B'
-            });
-          }
+      (async () => {
+        let resopnse = null;
+        /*
+        x = await (await fetch('https://rss.msn.com/ja-jp/?'+ym,
+               {method:'GET',cache: "no-cache", mode: "no-cors",credentials: 'include',
+               headers:{"Content-Type": "application/json; charset=utf-8",
+            }})).text();
+        */
+        //x = await (await fetch('https://connpass.com/api/v1/event/?count=100&ym=' + ym, {method:'GET',mode: "no-cors",credentials: 'include' }).then((response) => {return response;})).json();
+        // x = await response.text();
 
-          sessionStorage.setItem('event'+ym, JSON.stringify(events));
-          callback(events);
-      })
-      .fail(function(a,b) {
-          console.log(a);
-          console.log(b);
-      });
+        const x = await $.ajax("https://connpass.com/api/v1/event/?count=100&ym=" + ym,  {method:'GET',mode: "no-cors",credentials: 'include' }).then(response => response.text()).then(data => { console.log(data); return data; });
+      
+        console.log(x);
+        events = [];
+        events = connpass(x);
+        //
+        /*
+        response = await fetch('https://api.atnd.org/events/?count=100&ym=' + ym + '&start=1&format=jsonp', {method:'GET',mode: "no-cors",credentials: 'include'});
+        y = await response.text();
+        console.log(JSON.stringify(y));
+        event = atnd(JSON.stringify(y));
+        events.concat(event);
+        */
+        // 
+        // sessionStorage.setItem('event'+ym, JSON.stringify(events));
+        console.log(events);
+        callback(events);
+      })();
+
     }
 
     $('#calendar').fullCalendar({
@@ -111,4 +130,5 @@
       }
     });
     
-  });
+}, false);
+
