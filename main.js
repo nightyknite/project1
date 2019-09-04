0ec7496939929cc55924313675d316507450d2ff
nightyknite <nightyknite@gmail.com>
2019-09-05 05:56:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ document.addEventListener("DOMContentLoaded", function(){
       header    : {
         left  : 'prev,next today',
         center: 'title',
-        right : 'month,agendaWeek,agendaDay,list'
+        right : 'month,agendaWeek,agendaDay,listWeek,listDay'
       },
       buttonText: {
         today: 'today',
