25986b2b34a079fb7e3bf94161743df213fd7062
nightyknite <nightyknite@gmail.com>
2019-11-18 08:30:32 +0900

    fullcalendar v4

--- main.js
+++ main.js
@@ -2,13 +2,24 @@ document.addEventListener("DOMContentLoaded", function(){
     
     const setCalendar = (start, end, callback) => {
       let events = [];
-      const ym = start.add(7, 'days').format("YYYYMM"); 
+      let dt = new Date(start);
+      
+      dt.setDate(dt.getDate() + 7);
+
+      const ym = dt.getFullYear() + ("00" + (dt.getMonth()+1)).slice(-2);
       const item = sessionStorage.getItem('event' + ym);
-      if (item) {
+      
+      if (item !== null) {
         events = JSON.parse(item);
+
+        const keyword = document.getElementById("keyword");
+        if ((keyword !== null) && (keyword.value.length > 0)) {
+          events = events.filter(event => {return (event.description.indexOf(keyword.value) > 0);});
+        }
         callback(events);
         return;
       }
+      
       const connpass = data => {
         let event = [];
         for (var i in data.events) {
@@ -16,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function(){
             title: data.events[i].title,
             start: data.events[i].started_at,
             end: data.events[i].ended_at,
-            url: data.events.event_url,
+            url: data.events[i].event_url,
             description: ""
                          + "day:" + moment(data.events[i].started_at).format("MM/DD HH:mm") + " - "
                          + "" + moment(data.events[i].ended_at).format("MM/DD HH:mm") + "<br>"
@@ -58,69 +69,120 @@ document.addEventListener("DOMContentLoaded", function(){
         return event;
       }
 
+
+      const doorkeeper = data => {
+        let event = [];
+        for (let i in data) {
+          event.push({
+            title: data[i].event.title,
+            start: data[i].event.starts_at,
+            end: data[i].event.ends_at,
+            url: data[i].event.public_url,
+            description: ""
+                         + "day:" + moment(data[i].event.starts_at).format("MM/DD HH:mm") + " - "
+                         + "" + moment(data[i].event.ends_at).format("MM/DD HH:mm") + "<br>"
+                         + "limit:" + data[i].event.ticket_limit + "<br>"
+                         + "place:" + data[i].event.venue_name + "<br>"
+                         + "address:" + data[i].event.address + "<br>"
+                         + "description:" + data[i].event.description.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'').substring(0,49) + "<br>"
+                         + "",
+            backgroundColor: '#0F4FF4',
+            borderColor: '#EBAC2B',
+            textColor: 'white'
+          });
+        }
+        return event;
+      }
+
       (async () => {
         let data = [];
         let event = [];
+        const results = [];
+
         const progress = document.getElementById('eventloading');
+        progress.max = 15;
         progress.style.display = 'block';
         progress.value = 0;
-          
-        for (let i = 0; i < 10; i++) {
-          data = await $.ajax({url: 'https://connpass.com/api/v1/event/?count=100&ym=' + ym + '&start=' + (i * 100 + 1), dataType: 'jsonp'});
-          event = connpass(data);
-          events = events.concat(event);
-          progress.value = progress.value + 1;
-        }
         
-        for (let i = 0; i < 4; i++) {
-          data = await $.ajax({url: 'https://api.atnd.org/events/?count=100&ym=' + ym + '&start='+ (i * 100 + 1) + '&format=jsonp', dataType: 'jsonp'});          
-          event = atnd(data);
-          events = events.concat(event);
-          progress.value = progress.value + 1;
-        }
+        results.push((async () => {
+          for (let i = 0; i < 10; i++) {
+            data = await $.ajax({url: 'https://connpass.com/api/v1/event/?count=100&ym=' + ym + '&start=' + (i * 100 + 1), dataType: 'jsonp'});
+            event = connpass(data);
+            events = events.concat(event);
+            progress.value = progress.value + 1;
+          }
+        })());
+
+        results.push((async () => {
+          for (let i = 0; i < 4; i++) {
+            data = await $.ajax({url: 'https://api.atnd.org/events/?count=100&ym=' + ym + '&start='+ (i * 100 + 1) + '&format=jsonp', dataType: 'jsonp'});          
+            event = atnd(data);
+            events = events.concat(event);
+            progress.value = progress.value + 1;
+          }
+        })());
+        
+        results.push((async () => {
+          let doorkeeperToken = sessionStorage.getItem('doorkeeperToken') || localStorage.getItem('doorkeeperToken');
+          if (doorkeeperToken !== null) {
+            for (let i = 1; i < 20; i++) {
+              data = await $.ajax({url: 'https://api.doorkeeper.jp/events?since=' + moment(start).add(7, 'days').startOf('month').toISOString() + '&until=' + moment(start).add(7, 'days').endOf('month').toISOString() + '&sort=starts_at&page=' + i, dataType: 'jsonp', headers: { 'Authorization': 'Bearer ' +  doorkeeperToken} });     
+              event = doorkeeper(data);
+              events = events.concat(event);
+              progress.value = progress.value + 1;
+            }
+          }
+        })());
+
+        await Promise.all(results);
+        
         sessionStorage.setItem('event' + ym, JSON.stringify(events));
         progress.style.display = 'none';
         callback(events);
+
       })();
 
     }
-    
+
     const calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      header    : {
-        left  : 'prev,next today',
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+      plugins: [ 'dayGrid', 'list' ],
+      defaultView: 'dayGridMonth',
+      views: {
+        listDay: { buttonText: '日' },
+        listWeek: { buttonText: '週' }
+      },
+      header: {
+        left: 'prev,next today',
         center: 'title',
-        right : 'month,listWeek,listDay'
+        right: 'dayGridMonth,listWeek,listDay'
       },
-      buttonText: {
-        today: 'today',
-        month: 'month',
-        week : 'week',
-        day  : 'day'
-      },
-      axisFormat: 'HH:mm',
-      timeFormat: 'HH:mm',
-      timezone: 'local',
-      events    : function(start, end, timezone, callback) {
-        setCalendar(start, end, callback);        
-      },
-      editable  : true,
-      eventLimit: false,
-      selectable:true,
-      selectHelper:true,
-      eventRender: function(eventObj, $el) {
-        var content = eventObj.description || '';
-        $el.popover({
-          title: eventObj.title,
-          content: content,
+      locale: 'ja',
+      events: (info, successCallback, failureCallback) => setCalendar(info.startStr, info.endStr, successCallback),
+      eventRender: (info) => {
+        $(info.el).popover({
+          title: info.event.title,
+          content: info.event.extendedProps.description,
           trigger: 'hover',
           html: true,
           placement: 'top',
           container: 'body'
         });
+      },
+      eventClick: function(info) {
+        info.jsEvent.preventDefault();
+        if (info.event.url) {
+          window.open(info.event.url);
+        }
       }
     });
+
     calendar.render();
 
-}, false);
+    document.getElementById("search").addEventListener('click', ()=> {
+      calendar.refetchEvents();
+      calendar.render();
+    });
+    
+});
 
